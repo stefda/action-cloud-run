44bb85d3b9b5cce55042878b85334339f23c1cc6
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2020-08-28 13:56:24 +0700

    add cloudsql to input

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,18 @@ then
     ENVS=$(cat "$INPUT_ENV" | xargs | sed 's/ /,/g')
 fi
 
+
 if [ "$ENVS" ]
 then
     ENV_FLAG="--set-env-vars $ENVS"
 else
     ENV_FLAG="--clear-env-vars"
+fi
+
+
+if ["$INPUT_CLOUD_SQL"]
+then
+    ENV_FLAG="--add-cloudsql-instances $INPUT_CLOUD_SQL --update-env-vars INSTANCE_CONNECTION_NAME=$INPUT_CLOUD_SQL $ENV_FLAG"
 fi
 
 gcloud auth activate-service-account --key-file="$HOME"/gcloud.json --project "$INPUT_PROJECT"
